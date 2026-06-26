982d3047025639e463e81a83ceccce5a40f9f864
mjsamaha <matthewsamaha@gmail.com>
2026-06-26 06:45:02 -0400

    going to be changing the rendering architecture soon

--- StoneBound/src/com/lobsterchops/stonebound/game/Version.java
+++ StoneBound/src/com/lobsterchops/stonebound/game/Version.java
@@ -4,7 +4,7 @@ public final class Version {
 	
 	public static final String TITLE = "StoneBound";
 	
-	public static final String VERSION = "0.0.0";
+	public static final String VERSION = "0.0.1";
 	public static final String BUILD = "DEVELOPMENT";
 	
 
